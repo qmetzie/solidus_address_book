0bcdfec71ffbf5ff013e8677c3c3ef9549a1e46a
Okkiy Metta <okkiy.metta@bocistudio.com>
2018-09-13 16:58:50 +0700

    Fix decorator path

--- lib/solidus_address_book.rb
+++ lib/solidus_address_book.rb
@@ -19,7 +19,7 @@ module Solidus
       config.autoload_paths += %W(#{config.root}/lib)
 
       def self.activate
-        Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+        Dir.glob(File.join(File.dirname(__FILE__), '../app/**/*_decorator*.rb')) do |c|
           Rails.configuration.cache_classes ? require(c) : load(c)
         end
         Spree::Ability.register_ability(Spree::AddressAbility)
